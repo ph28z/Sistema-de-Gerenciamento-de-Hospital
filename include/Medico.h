99fb48b2982fb46e17932dd1c1ab3af4adf7595f
Pedro Zanchetta <phzanchetta@outlook.com>
2025-11-25 17:57:43 -0300

    Elaboração do repositório

---
+++ include/Medico.h
@@ -0,0 +1,13 @@
+//
+// Created by Pedro Zanchetta on 25/11/2025.
+//
+
+#ifndef SISTEMAHOSPITALAR_MEDICO_H
+#define SISTEMAHOSPITALAR_MEDICO_H
+
+
+class Medico {
+};
+
+
+#endif //SISTEMAHOSPITALAR_MEDICO_H
---
+++ src/Medico.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Pedro Zanchetta on 25/11/2025.
+//
+
+#include "Medico.h"
