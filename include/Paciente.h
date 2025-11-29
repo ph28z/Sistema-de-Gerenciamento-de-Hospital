261c75bc0e1f0aecf4753932f211afa29d7fd620
Dri0410 <137637647+barbizinhagameplay0410@users.noreply.github.com>
2025-11-29 20:30:18 -0300

    Update Paciente.h

--- include/Paciente.h
+++ include/Paciente.h
@@ -3,6 +3,9 @@
 
 #include "Pessoa.h"
 #include <vector>
+#include <string>
+
+using namespace std;
 
 class Paciente : public Pessoa {
 private:
@@ -14,28 +17,28 @@ private:
 public:
     // Construtor
     Paciente(const string& nome, const string& endereco, const string& telefone, int idade);
-
+    
     // Destrutor
     ~Paciente();
-
+    
     // Getters
     int getId() const;
     int getIdade() const;
     int getNumConsultas() const;
-
+    
     // Setter
     void setIdade(int i);
-
+    
     // Gerenciamento de consultas
     void adicionarConsulta(int idConsulta);
     void removerConsulta(int idConsulta);
     bool temConsultasAgendadas() const;
-
+    
     // Implementação do método virtual puro de Pessoa
     void imprimirDados() const override;
-
+    
     // Método adicional para exibir resumo em lista
     void exibirResumo() const;
 };
 
-#endif
+#endif 
